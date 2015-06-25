ff6ad7da1ea5fb74fa8cd66de6591cc254303ece
Ray Ward <ray.ward@bigcommerce.com>
2015-06-25 10:39:47 +1000

    add bigcommerce project

---
+++ modules/projects/manifests/bigcommerce.pp
@@ -0,0 +1,15 @@
+class projects::bigcommerce {
+  include homebrew
+  include php
+  include php::composer
+  include nodejs
+  include nvm
+  include vagrant
+  include virtualbox
+
+  boxen::project { 'bigcommerce':
+    source  => 'bigcommerce/bigcommerce-app-vm',
+    ruby    => '1.9.3',
+    php     => '5.6'
+  }
+}
