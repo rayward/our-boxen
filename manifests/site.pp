f3f91db2f01b8ed92680c6f6bc613da26a444115
Ray Ward <ray.ward@bigcommerce.com>
2015-06-26 09:51:03 +1000

    fix ruby version

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,7 +71,7 @@ node default {
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.2.6': }
+  ruby::version { '2.2.2': }
 
   # default php
   php::version { '5.6.9': }
