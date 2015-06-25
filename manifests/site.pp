8397c54df8e0e85cf7d47feff934aeae61ee9b17
Ray Ward <ray.ward@bigcommerce.com>
2015-06-25 10:29:13 +1000

    update deps in default site

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,7 +56,6 @@ node default {
   include dnsmasq
   include git
   include hub
-  include nginx
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -64,15 +63,11 @@ node default {
   }
 
   # node versions
-  nodejs::version { 'v0.6': }
-  nodejs::version { 'v0.8': }
-  nodejs::version { 'v0.10': }
+  nodejs::version { 'v0.12': }
 
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
   ruby::version { '2.1.2': }
 
   # common, useful packages
