e69d0c966fc753aa966734e022d1f8ac8b271da5
Ray Ward <ray.ward@bigcommerce.com>
2015-06-25 14:59:58 +1000

    move php to global config

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,6 +56,10 @@ node default {
   include dnsmasq
   include git
   include hub
+  include homebrew
+  include php
+  include php::composer
+  include php::extension::imagick
 
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
@@ -68,7 +72,10 @@ node default {
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.1.2': }
+  ruby::version { '2.2.6': }
+
+  # default php
+  php::version { '5.6': }
 
   # common, useful packages
   package {
--- modules/projects/manifests/bigcommerce.pp
+++ modules/projects/manifests/bigcommerce.pp
@@ -1,21 +1,10 @@
 class projects::bigcommerce {
-  include homebrew
-  include php
-  include php::composer
   include nodejs
   include vagrant
   include virtualbox
 
-  $repo_dir = "${boxen::config::srcdir}/bigcommerce"
-
   boxen::project { 'bigcommerce':
     source  => 'bigcommerce/bigcommerce-app-vm',
-    dir     => $repo_dir,
     ruby    => '1.9.3'
-  }
-
-  php::local { $repo_dir:
-    version => '5.6',
-    require => Boxen::Project['bigcommerce']
   }
 }
