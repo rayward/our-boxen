febbb3ec3d502fda99768894f1acaf267fae9326
Ray Ward <ray.ward@bigcommerce.com>
2015-06-26 11:20:03 +1000

    require bundler and bower

--- modules/projects/manifests/bigcommerce.pp
+++ modules/projects/manifests/bigcommerce.pp
@@ -6,8 +6,23 @@ class projects::bigcommerce {
     version => '1.7.1'
   }
 
+  $ruby_version = '1.9.3'
+  $node_version = 'v0.12'
+
+  ruby_gem { "bundler for ${ruby_version}":
+    gem          => 'bundler',
+    version      => '~> 1.8',
+    ruby_version => $ruby_version,
+    require      => Boxen::Project['bigcommerce']
+  }
+
+  nodejs::module { 'bower':
+    node_version => $node_version,
+    require      => Boxen::Project['bigcommerce']
+  }
+
   boxen::project { 'bigcommerce':
     source  => 'bigcommerce/bigcommerce-app-vm',
-    ruby    => '1.9.3'
+    ruby    => $ruby_version
   }
 }
